d09f43c60044940697bf59034710cec75397e1d3
Emre Surmeli <s.emre.s.8@gmail.com>
2015-04-22 16:04:49 -0700

    dryed up

--- ch05_exercises/flattening.js
+++ ch05_exercises/flattening.js
@@ -1,5 +1,4 @@
-var arrOfArrs = [[true, false], ["string"], [1, 2, 3]],
-    merged = [];
+var arrOfArrs = [[true, false], ["string"], [1, 2, 3]];
 
 arrOfArrs.reduce(function (val, merged) {
     return val.concat(merged);
