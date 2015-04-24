0507ddca4ce834b82c245e134eade7adb7ecc1bf
Emre Surmeli <s.emre.s.8@gmail.com>
2015-04-24 14:29:08 -0700

    q3

---
+++ ch05_exercises/historical_life_expectancy.js
@@ -0,0 +1,10 @@
+var whichCentury = forEach(ancestry, function (person) {
+  return Math.ceil(person.died / 100);
+});
+
+for (var century in whichCentury) {
+  var ages = whichCentury[century].map(function (person) {
+    return person.died - person.born;
+  });
+  console.log(century + "= " + average(ages));
+}
